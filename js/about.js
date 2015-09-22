5cc4fbf47cafeb5fd4ef36580c783e0e46a2da28
Nikhil Mashettiwar <nikhil@mashettiwar.com>
2015-09-22 05:14:45 -0400

    Added contact page. Finished logic for carousel on about page.

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.carousel-prev').click(function () {
-    console.log('prev clicked');
+
     var currPicture = parseInt($(this).parent().data("picture"));
     var newPicture;
     if (currPicture === 1) {
@@ -43,5 +43,27 @@ $(document).ready(function () {
     $(".thumbnail" + newPicture).addClass('thumbnail-active');
 
   });
+
+  $(".thumbnail").click(function () {
+    if ($(this).hasClass('thumbnail-active')) {
+      console.log('already active');
+      return;
+    }
+    var $selectedPicture = $('.selected-picture')
+    var currPicture = parseInt($selectedPicture.data("picture"));
+    var newPicture = $(this).data("picture");
+
+    //Set Data of New Picture
+    $selectedPicture.data("picture", newPicture);
+
+    //Remove CSS of old picture
+    $selectedPicture.removeClass('picture-' + currPicture);
+    $('.thumbnail-active').removeClass('thumbnail-active');
+
+    //Set CSS of new picture
+    $selectedPicture.addClass('picture-' + newPicture);
+    $(this).addClass('thumbnail-active');
+  });
+
 
 });

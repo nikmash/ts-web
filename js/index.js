62191e389e44f223679e9005948c3305b5c48812
Nikhil Mashettiwar <nikhil@mashettiwar.com>
2015-09-23 21:26:39 -0400

    style changes

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 		var headerpos = 50;
 		var pos = $(window).scrollTop();
 		if(pos > headerpos) {
-		    $('.header').css({
+		    $('.header-landing').css({
 		      'background-color': 'rgba(0,0,0,0.7)',
 		      'transition': 'background-color 0.3s',
 		      '-webkit-transition': 'background-color 0.3s'
 		    });
 		} else {
-		    $('.header').css({
+		    $('.header-landing').css({
 		    	'background-color': 'inherit',
 		    	'transition': 'background-color 0.3s',
 		    	'-webkit-transition': 'background-color 0.3s'
@@ -47,6 +47,38 @@ $(document).ready(function() {
 			}
 		]
 	});
+
+	$('a.login-window').click(function() {
+
+  	//Getting the variable's value from a link
+    var loginBox = $(this).attr('href');
+
+    //Fade in the Popup
+    $(loginBox).fadeIn(300);
+
+    //Set the center alignment padding + border see css style
+    var popMargTop = ($(loginBox).height() + 24) / 2;
+    var popMargLeft = ($(loginBox).width() + 24) / 2;
+
+    $(loginBox).css({
+        'margin-top' : -popMargTop,
+        'margin-left' : -popMargLeft
+    });
+
+    // Add the mask to body
+    $('body').append('<div id="mask"></div>');
+    $('#mask').fadeIn(300);
+
+    return false;
+	});
+
+	// When clicking on the button close or the mask layer the popup closed
+	// $('a.close, #mask').live('click', function() {
+	//   $('#mask , .login-popup').fadeOut(300 , function() {
+	//     $('#mask').remove();
+	// 	});
+	// 	return false;
+	// });
 
 	// Slick Carousel
 	// $('.slick-carousel').slick({
